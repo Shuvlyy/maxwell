74eb41be4edc5ac4c66440c6c942f7424c159c3d
Lysandre B. <lysandre.boursette@epitech.eu>
2026-08-31 18:40:37 +0200

    chore: Changed ports

--- app/lib/core/api/base_url_provider.dart
+++ app/lib/core/api/base_url_provider.dart
@@ -6,7 +6,7 @@ part 'base_url_provider.g.dart';
 @Riverpod(keepAlive: true)
 class BaseUrl extends _$BaseUrl {
   static const _key = 'api_base_url';
-  static const _defaultUrl = 'http://shuvly.freeboxos.fr:50067';
+  static const _defaultUrl = 'http://shuvly.freeboxos.fr:50068';
 
   @override
   String build() {
